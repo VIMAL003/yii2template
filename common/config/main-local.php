91a08bb3a1cba87dd49355acc909ed87860bd44a
vimal patel <vimalpatel30989@gmail.com>
2016-05-05 15:37:15 +0530

    still not remove vendor folder from commit

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -14,7 +14,15 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => FALSE,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'rsplajayg@gmail.com',
+                'password' => 'rspl123#',
+                'port' => '587', // Port 25 is a very common port too
+                'encryption' => 'tls', // It is often used, check your provider or mail server specs
+            ],
         ],
     ],
 ];
