461a3b11183470bf3138afdff014db52eb9c4e36
vimal patel <vimalpatel30989@gmail.com>
2016-06-16 11:34:46 +0530

    Main-local file commit

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -18,8 +18,8 @@ return [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'viki51188@gmail.com',
-                'password' => 'viki123654',
+                'username' => 'vimal.patel@gmail.com',
+                'password' => 'rspl123#',
                 'port' => '587', // Port 25 is  a very common port too
                 'encryption' => 'tls', // It is often used, check your provider or mail server specs
             ],
